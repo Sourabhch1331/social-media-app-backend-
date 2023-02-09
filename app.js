e4db50bf6d1c61e20a76ca51e81567b27ddaeb38
Sourabh Choudhary <Sourabhch1331@gmail.com>
2023-02-09 21:58:39 +0530

    Added Web security to secure web page from NoSQL Injections, Data Sanitization

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const compression = require('compression');
 const path = require('path');
 const morgan = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
 
+const AppError = require('./utils/appError');
 const userRoutes = require('./routes/userRoutes');
 const postRouter = require('./routes/postRouter');
 
@@ -14,9 +19,34 @@ const app = express();
 // serving files from public
 app.use(express.static(path.join(__dirname, 'public')));  
 
+// Set Security http headers
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'", 'http://127.0.0.1:3000/*'],
+        baseUri: ["'self'"],
+        fontSrc: ["'self'", 'https:', 'data:'],
+        scriptSrc: ["'self'", 'https://*.cloudflare.com' ],
+        scriptSrc: ["'self'", 'https://*.stripe.com', 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js'],
+        frameSrc: ["'self'", 'https://*.stripe.com'],
+        objectSrc: ["'none'"],
+        styleSrc: ["'self'", 'https:', 'unsafe-inline'],
+        upgradeInsecureRequests: [],
+      },
+    })
+  );
+
 
 // body parser
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true, limit: '10kb'}));
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
 
 // compressing data 
 app.use(compression());
@@ -31,8 +61,11 @@ if(process.env.Node_ENV === 'development'){
 
 app.use('/api/v1/user',userRoutes);
 app.use('/api/v1/post',postRouter);
-app.use('/',(req,res)=>{
-    res.send('hello from server');
+
+
+
+app.all('*',(req,res,next)=>{
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`,404));
 });
 
 app.use(globalErrorHandler);
